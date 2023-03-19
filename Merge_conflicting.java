8031b7dfff73f900bf10c5aedaa75bb9346f549e
Alex Bril <brilsasha@gmail.com>
2023-03-19 12:31:28 +0200

    "Second_branch_for_merge_conflict"

--- Merge_conflicting.java
+++ Merge_conflicting.java
@@ -1,5 +1,6 @@
 public class Merge_conflicting {
     public static void main(String[] args) {
         System.out.println("Hello world! I'm Alex");
+        System.out.println("World is beautiful");
     }
 }
