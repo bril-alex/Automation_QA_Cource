5831677243fd53dec0cec00bddb4bfbb597eeea3
bril-alex <112894050+bril-alex@users.noreply.github.com>
2023-03-19 12:39:22 +0200

    Delete Merge_conflicting.java

--- Merge_conflicting.java
+++
@@ -1,7 +0,0 @@
-public class Merge_conflicting {
-    public static void main(String[] args) {
-        System.out.println("Hello world! I'm Alex");
-        System.out.println("I wanna be an Automation QA");
-        System.out.println("World is beautiful");
-    }
-}
